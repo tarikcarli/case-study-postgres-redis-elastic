407439603aaf3f33c10917bb5bc08013ec35b863
Tarık Çarlı <pro.tarikcarli@gmail.com>
2022-08-06 19:00:57 +0300

    open interval to sync data between elastic and redis

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 connectPostgres();
 connectRedis();
 connectElastic();
-// interval();
+interval();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "5mb" }));
